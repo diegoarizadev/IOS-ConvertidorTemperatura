a2c0c02001e773282b937394f4ebbe5055b518db
n0rfen <DiegoAriza@MacBook-Pro-de-n0rf3n.local>
2016-02-07 16:13:04 -0500

    Convertidor de Temperatura
    Desarrollo realizado en Objective-C - Certificación IOS EAN.

---
+++ Convertidor Temperaturas/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Convertidor Temperaturas
+//
+//  Created by n0rf3n on 7/05/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Convertidor Temperaturas/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  Convertidor Temperaturas
+//
+//  Created by n0rf3n on 7/05/15.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
